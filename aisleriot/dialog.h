59fd85aab51cc43b38b3e15ecb9cb0242d58cada
rhlabs <rhlabs>
1998-04-22 18:58:28 +0000

    Fixed a prototype.
    Dr Mike <msf@redhat.com>

--- aisleriot/dialog.h
+++ aisleriot/dialog.h
@@ -3,7 +3,7 @@
 #include "gnome.h"
 
 int hide_game_over_box_callback (GtkWidget *app, void *data );
-int hide_select_box_callback (GtkWidget *app, void *data );
+void hide_select_box_callback (GtkWidget *app, void *data );
 void hide_game_over_box();
 void hide_select_box();
 void show_game_over_dialog(gboolean);
