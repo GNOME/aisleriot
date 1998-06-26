5b20ce12b11210fc9af54b1e7ba4ae1787f042c4
Scott D Heavner <sdh@src.gnome.org>
1998-06-26 22:28:01 +0000

    Autodetect curses for cyahtzee.

--- acconfig.h
+++ acconfig.h
@@ -13,3 +13,32 @@
 /* Define this if you want to compile against the development branch of gtk */
 #undef HAVE_DEVGTK
 
+
+/*=== Curses variables swiped from Midnight commander's config ===*/
+   
+/* Found some version of curses that we're going to use */
+#undef HAS_CURSES
+
+/* Are you using other type of curses? */
+#undef OTHER_CURSES
+
+/* Use SunOS SysV curses? */
+#undef SUNOS_CURSES
+
+/* Use old BSD curses? */
+#undef USE_BSD_CURSES
+
+/* Use SystemV curses? */
+#undef USE_SYSV_CURSES
+
+/* Use Ncurses? */
+#undef USE_NCURSES
+
+/* If you Curses does not have color define this one */
+#undef NO_COLOR_CURSES
+
+/* Define if you want to turn on SCO-specific code */
+#undef SCO_FLAVOR
+
+/* Version of ncurses */
+#undef NCURSES_970530
