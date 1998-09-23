c15912215457a0a470d63dd761398abaf25f0fbd
Felix Bellaby <felix@src.gnome.org>
1998-09-23 19:45:28 +0000

    (main_prog) scans dir for installed games (start_timer) (stop_timer) C level control over timer. (timer_cb) updates time display, executes any scheme callback and checks for end of game.

--- aisleriot/sol.c
+++ aisleriot/sol.c
@@ -16,8 +16,6 @@
  * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
-#define SOL_C
-
 #include <config.h>
 
 #include <sys/types.h>
@@ -36,25 +34,26 @@
 #include "cscmi.h"
 #include "menu.h"
 /*
- * Variables
+ * Global Variables
  */
-
-GtkWidget *app;
-GtkWidget *status_bar;
-GtkWidget *playing_area;
-GdkGC *draw_gc;  /* needed for tiling operations */
-GdkPixmap *surface = NULL;
-GdkPixmap *moving_card_pixmap;
-gint score;
-GtkWidget *score_value;
-gint seed;
-gchar* game_file;
-gchar* game_name;
-press_data_type* press_data = NULL; 
+GtkWidget        *app;
+GtkWidget        *playing_area;
+GdkGC            *draw_gc;
+GdkPixmap        *surface;
+GdkPixmap        *moving_card_pixmap;
+guint            score;
+guint            game_seconds;
+guint            timeout;
+guint            seed;
+guint            n_games;
+struct dirent    **game_dents;
+gchar            *game_file;
+gchar            *game_name;
+press_data_type* press_data; 
 
 /*
  * This function does i18n on game files using the convention that
- * filenames are C locale names with mechanical changes:
+ * filenames are LC_MESSAGES with mechanical changes:
  */
 
 gchar* game_file_to_name (const gchar* file)
@@ -63,7 +62,6 @@ gchar* game_file_to_name (const gchar* file)
 
   if ((p = strrchr (buf, '.'))) *p = '\0';
   for(p = buf; p = strchr(p, '_'), p && *p;) *p = ' ';
-  /* FIXME : drop next hack by renaming the files */
   buf[0] = toupper(buf[0]);
   p = g_strdup(_(buf));
 
@@ -71,12 +69,24 @@ gchar* game_file_to_name (const gchar* file)
   return p;
 }
 
+GtkWidget *score_value;
+
 void set_score () 
 {
   char b [10];
-  sprintf (b, "%6d ", score);
+  sprintf (b, "%6d  ", score);
 
   gtk_label_set(GTK_LABEL(score_value), b);
+}
+
+GtkWidget *time_value;
+
+void set_time () 
+{
+  char b [10];
+  sprintf (b, "%3d:%02d ", game_seconds / 60, game_seconds % 60);
+
+  gtk_label_set(GTK_LABEL(time_value), b);
 }
 
 void make_title () 
@@ -86,6 +96,36 @@ void make_title ()
   g_string_sprintfa (title, "  ( %d )", seed);
 
   gtk_window_set_title (GTK_WINDOW (app), title->str); 
+}
+
+gint timer_cb ()
+{
+  game_seconds++;
+  set_time();
+  if (game_seconds > timeout) {
+    timeout = 3600;
+    gh_apply(game_data->timeout_lambda, SCM_EOL);
+    end_of_game_test();
+  }
+  return game_seconds < 3599; /* give up at end of one hour */
+}
+
+guint timer_timeout = 0;
+
+void timer_start ()
+{
+  if (timer_timeout)
+    timer_stop();
+  game_seconds = 0;
+  timeout = 3600;
+  set_time();
+  timer_timeout = gtk_timeout_add (1000, (GtkFunction) (timer_cb), NULL); 
+}
+
+void timer_stop ()
+{
+  gtk_timeout_remove (timer_timeout);
+  timer_timeout = 0;
 }
 
 /*
@@ -114,11 +154,11 @@ void create_sol_board ()
   gdk_gc_set_fill (draw_gc, GDK_TILED);
   
   gtk_signal_connect (GTK_OBJECT(playing_area),"button_release_event",
-		      (GtkSignalFunc) button_release_event, NULL);
+		      GTK_SIGNAL_FUNC (button_release_event), NULL);
   gtk_signal_connect (GTK_OBJECT (playing_area), "motion_notify_event",
-		      (GtkSignalFunc) motion_notify_event, NULL);
+		      GTK_SIGNAL_FUNC (motion_notify_event), NULL);
   gtk_signal_connect (GTK_OBJECT (playing_area), "button_press_event",
-		      (GtkSignalFunc) button_press_event, NULL);
+		      GTK_SIGNAL_FUNC (button_press_event), NULL);
 }
 
 void create_main_window ()
@@ -126,12 +166,12 @@ void create_main_window ()
   app = gnome_app_new ("solitaire", _("Solitaire"));
   gtk_widget_realize (app);
 
-  gtk_signal_connect (GTK_OBJECT(app), "delete_event", 
-		      GTK_SIGNAL_FUNC(file_quit_callback), NULL);
+  gtk_signal_connect (GTK_OBJECT (app), "delete_event", 
+		      GTK_SIGNAL_FUNC (file_quit_callback), NULL);
   gtk_signal_connect (GTK_OBJECT (app), "configure_event",
-		      (GtkSignalFunc) configure_event, NULL);
+		      GTK_SIGNAL_FUNC (configure_event), NULL);
 
-  gtk_window_set_policy (GTK_WINDOW(app), TRUE, TRUE, FALSE);
+  gtk_window_set_policy (GTK_WINDOW (app), TRUE, TRUE, FALSE);
 }
 
 void create_press_data ()
@@ -141,8 +181,8 @@ void create_press_data ()
   attributes.wclass = GDK_INPUT_OUTPUT;
   attributes.window_type = GDK_WINDOW_CHILD;
   attributes.event_mask = 0;
-  attributes.width = 1;
-  attributes.height = 1;
+  attributes.width = get_card_width();
+  attributes.height = get_card_height();
   attributes.colormap = gdk_window_get_colormap (playing_area->window);
   attributes.visual = gdk_window_get_visual (playing_area->window);
   
@@ -151,9 +191,6 @@ void create_press_data ()
   press_data->moving_cards = gdk_window_new(playing_area->window, &attributes,
 					    (GDK_WA_VISUAL | GDK_WA_COLORMAP));
 }
-/*
- * main()
- */
 
 void eval_installed_file (char *file)
 {
@@ -165,48 +202,39 @@ void eval_installed_file (char *file)
     return;
   }
   
-  relative = g_copy_strings ("sol-games/", file, NULL);
+  relative = g_copy_strings (GAMESDIR, file, NULL);
   installed_filename = gnome_datadir_file (relative);
   gh_eval_file (installed_filename);
   g_free (installed_filename);
   g_free (relative);
 }
 
+int
+is_game (struct dirent* dent)
+{
+  return (!strcmp (g_extension_pointer (dent->d_name),"scm") &&
+	  strcmp (dent->d_name,"sol.scm"));
+}
+
 void main_prog(int argc, char *argv[])
 {
-  GtkWidget *score_label, *score_box;
+  GtkWidget *score_label, *time_label, *score_box, *status_bar;
+  char* dir;
   
   bindtextdomain (PACKAGE, GNOMELOCALEDIR);
   textdomain (PACKAGE);
+  //argp_program_version = VERSION;
+
+  dir = gnome_datadir_file (GAMESDIR);
+  n_games = scandir (dir, &game_dents, is_game, alphasort);
+  g_free(dir);
 
   gnome_init ("aisleriot", NULL, argc, argv, 0, NULL);
 
   seed = time(NULL);
   srandom(seed);
 
-  /* Scheme stuff... */
-  /* FIXME: On 1997-11-14, gh_enter stopped loading `icd-9/boot-9.scm'.
-     In my copy of guile, the first define in boot-9.scm is for "provide",
-     and it looked as good a test as any  */
-  gh_eval_str ("(if (not (defined? \'provide))\n"
-	       "  (primitive-load-path \"ice-9/boot-9.scm\"))");
-  gh_new_procedure0_0("get-card-width", scm_get_card_width);
-  gh_new_procedure0_0("get-card-height", scm_get_card_height);
-  gh_new_procedure0_0("get-horiz-offset",scm_get_horiz_offset);
-  gh_new_procedure0_0("get-vert-offset", scm_get_vert_offset);
-  gh_new_procedure0_0("get-horiz-start", scm_get_horiz_start);
-  gh_new_procedure0_0("get-vert-start", scm_get_vert_start);
-  gh_new_procedure1_0("set-surface-layout", scm_set_surface_layout);
-  gh_new_procedure0_0("reset-surface", scm_reset_surface);
-  gh_new_procedure1_0("add-slot", scm_add_slot);
-  gh_new_procedure1_0("get-slot", scm_get_slot);  
-  gh_new_procedure2_0("set-cards!", scm_set_cards);
-  gh_new_procedure0_0("get-score", scm_get_score);  
-  gh_new_procedure1_0("set-score!", scm_set_score);
-  gh_new_procedure1_0("add-to-score!", scm_add_to_score);
-  gh_new_procedure("set-lambda", scm_set_lambda, 8, 0, 0);
-  gh_new_procedure1_0("random", scm_random);
-  eval_installed_file ("sol.scm");
+  cscm_init();
 
   create_main_window ();
 
@@ -216,12 +244,19 @@ void main_prog(int argc, char *argv[])
   create_menus (GNOME_APP(app));
 
   score_box = gtk_hbox_new(0, FALSE);
-  score_label = gtk_label_new (_("Score: "));
+  score_label = gtk_label_new (_("Score:"));
   gtk_box_pack_start (GTK_BOX(score_box), score_label, FALSE, FALSE, 0);
-  score_value = gtk_label_new ("     0 ");
+  score_value = gtk_label_new ("     0  ");
   gtk_box_pack_start (GTK_BOX(score_box), score_value, FALSE, FALSE, 0);
+  time_label = gtk_label_new (_("Time:"));
+  gtk_box_pack_start (GTK_BOX(score_box), time_label, FALSE, FALSE, 0);
+  time_value = gtk_label_new ("  0:00 ");
+  gtk_box_pack_start (GTK_BOX(score_box), time_value, FALSE, FALSE, 0);
+
   gtk_widget_show (score_label);
   gtk_widget_show (score_value);
+  gtk_widget_show (time_label);
+  gtk_widget_show (time_value);
   gtk_widget_show (score_box);
 
   status_bar = gnome_appbar_new (FALSE, TRUE, TRUE);
