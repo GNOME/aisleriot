6f11ae97a3ff73149c713fe5c131994e04d1e2bd
Felix Bellaby <felix@src.gnome.org>
1998-10-26 23:20:33 +0000

    library updated to imlib

--- freecell/gdk-card-image/gdk-card-image.h
+++ freecell/gdk-card-image/gdk-card-image.h
@@ -1,4 +1,4 @@
-/* card-image.h -- card image loading library.  */
+/* gdk-card-image.h -- card image loading library.  */
 
 #ifndef __GDK_CARD_IMAGE_H
 #define __GDK_CARD_IMAGE_H
@@ -6,6 +6,13 @@
 #include <gdk/gdk.h>
 
 void gdk_card_image_init (GdkWindow *window);
+void gdk_card_image_unref ();
+
+GdkPixmap* gdk_card_image_face (int suit, int value);
+GdkPixmap* gdk_card_image_back ();
+GdkBitmap* gdk_card_image_mask ();
+
+/* Obsolete */
 void gdk_card_image (int card_id, GdkPixmap **pixmap, GdkBitmap **mask);
 
 #endif /* __GDK_CARD_IMAGE_H */
