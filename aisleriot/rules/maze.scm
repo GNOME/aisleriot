9db5522a62ed7757c4f7c9e28f628c1f6f023ea4
Rosanna Yuen <zana@webwynk.net>
2001-02-19 21:06:53 +0000

    Matthew Wilcox's new maze version; fixes undo and scoring.
    Mon Feb 19 17:06:50 2001  Rosanna Yuen  <zana@webwynk.net>
    
    	* maze.scm: Matthew Wilcox's new maze version; fixes undo and
    	scoring.

--- aisleriot/rules/maze.scm
+++ aisleriot/rules/maze.scm
@@ -16,9 +16,7 @@
 ; Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
 ; USA
 
-(define BASE-VAL 0)
-
-(define score 0)
+(define card '())
 
 (define (add-line)
   (add-normal-slot '())
@@ -30,8 +28,6 @@
   (add-normal-slot '())
   (add-normal-slot '())
   (add-normal-slot '())
-;  (add-normal-slot '())
-;  (add-normal-slot '())
 )
 
 (define (new-game)
@@ -49,7 +45,6 @@
   (add-normal-slot '())
   (add-normal-slot '())
   (add-normal-slot '())
-;  (add-normal-slot '())
   (add-carriage-return-slot)
 
   (add-line)
@@ -62,20 +57,17 @@
   (add-carriage-return-slot)
   (add-line)
 
-  (deal-cards-face-up 0 '(    1  2  3  4  5  6  7
-			   9 10 11 12 13 14 15 16
-			  18 19 20 21 22 23 24 25 26
-			  27 28 29 30 31 32 33 34 35
-			  36 37 38 39 40 41 42 43 44
-			  45 46 47 48 49 50 51 52 53))
+  (deal-cards-face-up 0 '(1 2 3 4 5 6 7 9 10 11 12 13 14 15 16 18 19
+			    20 21 22 23 24 25 26 27 28 29 30 31 32 33 
+			    34 35 36 37 38 39 40 41 42 43 44 45 46 47 
+			    48 49 50 51 52 53))
+
   (make-visible-top-card 0)
   (eliminate-kings 53)
 
-  (give-status-message)
+  (calculate-score)
 
-  (list 11 5)
-  (list 9 6)
-)
+  (list 9 6))
 
 (define (eliminate-kings slot)
   (and (not (empty-slot? slot))
@@ -84,51 +76,26 @@
   (and (> slot 0)
        (eliminate-kings (- slot 1))))
 
-(define (give-status-message)
-  (set-statusbar-message (string-append "Score: " (number->string score)))
-  #f)
-
 (define (button-pressed slot-id card-list)
   #t)
 
-(define (card-val card-list)
-  (get-value (car card-list)))
-
-(define (card-suit card-list)
-  (get-suit (car card-list)))
-
-(define (card-inferior-value slot)
-  (cond ((empty-slot? (- slot 1)) -1)
-        (#t (get-value (get-top-card (- slot 1))))))
-
-(define (card-superior-value slot)
-  (cond ((empty-slot? (+ slot 1)) -1)
-        (#t (get-value (get-top-card (+ slot 1))))))
-
-(define (card-inferior-suit slot)
-  (cond ((= slot 0) -1)
-        ((empty-slot? (- slot 1)) -1)
-        (#t (get-suit (get-top-card (- slot 1))))))
-
-(define (card-superior-suit slot)
-  (cond ((= slot 53) -1)
-        ((empty-slot? (+ slot 1)) -1)
-        (#t (get-suit (get-top-card (+ slot 1))))))
+(define (card-next? lower higher)
+  (or (and (= ace (get-value higher))
+	   (= queen (get-value lower)))
+      (suit-next? lower higher)))
 
 (define (button-released start-slot card-list end-slot)
-  (cond ((not (empty-slot? end-slot)) #f)
-        ((or (and (= (card-val card-list) ace)
-                  (or (= end-slot 0)
-		      (= (card-inferior-value end-slot) queen)))
-               (and (= (card-val card-list) queen)
-	          (or (= end-slot 53)
-		      (= (card-superior-value end-slot) ace)))
-	     (and (= (card-inferior-suit end-slot) (card-suit card-list))
-	          (= (card-inferior-value end-slot) (- (card-val card-list) 1)))
-	     (and (= (card-superior-suit end-slot) (card-suit card-list))
-	          (= (card-superior-value end-slot) (+ (card-val card-list) 1))))
-	  (move-n-cards! start-slot end-slot card-list))
-        (#t #f)))
+  (set! card (car card-list))
+  (and (empty-slot? end-slot)
+       (or (if (= end-slot 0)
+	       (= ace (get-value card))
+	       (and (not (empty-slot? (- end-slot 1)))
+		    (card-next? (get-top-card (1- end-slot)) card)))
+	   (if (= end-slot 53)
+	       (= queen (get-value card))
+	       (and (not (empty-slot? (1+ end-slot)))
+		    (card-next? card (get-top-card (1+ end-slot))))))
+       (add-card! end-slot (car card-list))))
 
 (define (button-clicked slot-id)
   #f)
@@ -136,30 +103,35 @@
 (define (button-double-clicked slot-id)
   #f)
 
-(define (in-order slot card suit)
-  (set! score slot)
-  (give-status-message)
-  (cond ((= slot 54) #t)
-  	((empty-slot? slot) (in-order (+ slot 1) card suit))
-  	(#t (in-order-helper slot card
-  			(if (= card ace) (get-suit (get-top-card slot)) suit)))
-  )
+(define (get-full-card slot)
+  (if (empty-slot? slot)
+      (get-full-card (1+ slot))
+      (get-top-card slot)))
+
+(define (calculate-score)
+  (set! card (get-full-card 0))
+  (if (= (get-value card) ace)
+      (set-score! 1)
+      (set-score! 0))
+  (calculate-score-helper 1 card)
+  (= (get-score) 48) ; 48 cards in the pack
 )
 
-(define (in-order-helper slot card suit)
-  (cond
-	((not (= (get-value (get-top-card slot)) card)) #f)
-	((not (= (get-suit (get-top-card slot)) suit)) #f)
-	(#t (in-order (+ slot 1) (+ (remainder card 12) 1) suit))
-  )
-)
+(define (calculate-score-helper slot prev)
+  (or (= slot 54)
+      (and (empty-slot? slot)
+	   (calculate-score-helper (1+ slot) prev))
+      (and (set! card (get-top-card slot))
+	   (card-next? prev card)
+	   (add-to-score! 1)
+	   #f)
+      (calculate-score-helper (1+ slot) card)))
 
 (define (game-won)
-  (in-order 0 ace -1))
+  (calculate-score))
 
 (define (game-over)
-	(not (game-won))
-)
+  (not (game-won)))
 
 (define (get-hint)
   (list 0 "Aim to place the suits in the order which fits the current layout most naturally."))
