17e6eefb712707a753f5a00a3473f2e064028082
Jonathan Blandford <jrb@src.gnome.org>
2001-07-19 05:55:35 +0000

    Thu Jul 19 01:50:53 2001  Jonathan Blandford  <jrb@redhat.com>>
    	* aisleriot.sgml: remove block-ten docs.
    	* Makefile.am: remove block-ten file from installation until it is
    	fixed.
    	* agnes.scm: add (theoretical) winning game to seed.

--- aisleriot/rules/agnes.scm
+++ aisleriot/rules/agnes.scm
@@ -15,7 +15,9 @@
 ; along with this program; if not, write to the Free Software
 ; Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
 ; USA
-
+;
+;
+; Andersca claims that seed 1791329065 wins
 (define BASE-VAL 0)
 
 (define (new-game)
