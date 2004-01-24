6e885d731d16fbea93094991f982550c80d486ef
Callum McKenzie <callum@src.gnome.org>
2004-01-24 10:07:02 +0000

    More aisleriot statistics work.

--- aisleriot/statistics.c
+++ aisleriot/statistics.c
@@ -23,8 +23,23 @@
 #include "sol.h"
 #include "statistics.h"
 
+typedef struct _game_stats {
+  guint wins;
+  guint total;
+  guint best;
+  guint worst;
+} game_stats;
+
+GHashTable * stats = NULL;
+game_stats * current_stats;
+
 GtkWidget * statistics_dialog = NULL;
 GtkWidget * name_label;
+GtkWidget * wins_label;
+GtkWidget * total_label;
+GtkWidget * percentage_label;
+GtkWidget * best_label;
+GtkWidget * worst_label;
 
 /* To remove some warnings, these are linked in at run time by libglade. */
 gboolean close_statistics_dialog (GtkWidget * widget);
@@ -53,15 +68,60 @@ close_statistics_dialog (GtkWidget * widget)
 
 void update_statistics_display (void)
 {
-  gchar * name;
+  gchar * text;
 
   if (!statistics_dialog || !GTK_WIDGET_VISIBLE (statistics_dialog))
     return;
 
-  name = g_strdup_printf ("<b>%s</b>", game_name);
-  gtk_label_set_text (GTK_LABEL (name_label), name);
+  text = g_strdup_printf ("<b>%s</b>", game_name);
+  gtk_label_set_text (GTK_LABEL (name_label), text);
   gtk_label_set_use_markup (GTK_LABEL (name_label), TRUE);
-  g_free (name);
+  g_free (text);
+
+  if (!stats) {
+    stats = g_hash_table_new (g_str_hash, g_str_equal);
+  }
+
+  /* Get the current stats from the hash table. Create a new 
+   * entry if there are no stats. */
+  current_stats = g_hash_table_lookup (stats, game_name);
+  if (!current_stats) {
+    current_stats = g_new (game_stats, 1);
+    current_stats->wins = 0;
+    current_stats->total = 0;
+    current_stats->best = 0;
+    current_stats->worst = 0;
+    g_hash_table_insert (stats, game_name, current_stats);
+  }
+
+  text = g_strdup_printf ("%d", current_stats->wins);
+  gtk_label_set_text (GTK_LABEL (wins_label), text);
+  g_free (text);
+
+  text = g_strdup_printf ("%d", current_stats->total);
+  gtk_label_set_text (GTK_LABEL (total_label), text);
+  g_free (text);
+
+  if (current_stats->total != 0)
+    text = g_strdup_printf ("% 3d%%", 
+			    (100*current_stats->wins)/current_stats->total);
+  else
+    /* For translators: N/A means "Not Applicable", use whatever
+     * abbreviation you have for a value that has no meaning. */
+    text = g_strdup (_("N/A"));
+  gtk_label_set_text (GTK_LABEL (percentage_label), text);
+  g_free (text);
+
+  /* FIXME: Make these two pritn actual times. */
+  text = g_strdup_printf ("%d", current_stats->best);
+  gtk_label_set_text (GTK_LABEL (best_label), text);
+  g_free (text);
+
+  text = g_strdup_printf ("%d", current_stats->worst);
+  gtk_label_set_text (GTK_LABEL (worst_label), text);
+  g_free (text);
+
+
 }
 
 void
@@ -74,6 +134,11 @@ show_statistics_dialog (void)
     glade_xml_signal_autoconnect (dialog);
     statistics_dialog = glade_xml_get_widget (dialog, "Statistics");
     name_label = glade_xml_get_widget (dialog, "name_label");
+    wins_label = glade_xml_get_widget (dialog, "wins_label");
+    total_label = glade_xml_get_widget (dialog, "total_label");
+    percentage_label = glade_xml_get_widget (dialog, "percentage_label");
+    best_label = glade_xml_get_widget (dialog, "best_label");
+    worst_label = glade_xml_get_widget (dialog, "worst_label");
   } else {
     gtk_window_present (GTK_WINDOW (statistics_dialog));
   }
