ea7b15c1ec2296aa11542d94c3dd7257263aee0b
Callum McKenzie <callum@src.gnome.org>
2005-03-08 09:35:06 +0000

    Fix a bug with clock patience and click-to-move mode (how pointless is that?)

--- aisleriot/rules/clock.scm
+++ aisleriot/rules/clock.scm
@@ -72,7 +72,8 @@
 
 (define (button-pressed slot-id card-list)
   (and (= slot-id 6)
-       (= (length card-list) 1)))
+       (= (length card-list) 1)
+       (not (= king (get-value (car card-list))))))
 
 (define (transaction-good? end-slot card-list)
   (or (and (= end-slot 2)
