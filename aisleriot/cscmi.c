d4cf1aae01700777a02df0972e4ca31ea061b6d2
Jason Clinton <jclinton@src.gnome.org>
2006-07-20 12:53:04 +0000

    really fix #347679 this time :)

--- aisleriot/cscmi.c
+++ aisleriot/cscmi.c
@@ -196,7 +196,7 @@ cscmi_catch_handler (gpointer data,
   cscmi_write_exception_details (error_fd, tag, throw_args);
   close (error_fd);
 
-  exec_str = g_strdup_printf ("bug-buddy --package=gnome-games --package-version=%s --appname=aisleriot --pid=%d --kill=%d --include=%s",
+  exec_str = g_strdup_printf ("bug-buddy --package=gnome-games --package-ver=%s --appname=aisleriot --pid=%d --kill=%d --include=%s",
 			      VERSION,
 			      getpid (),
 			      getpid (),
