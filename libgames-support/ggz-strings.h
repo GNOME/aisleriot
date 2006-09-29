e815c62d0b6d49ca1186e4f1d68edad736dd53a2
Andreas Røsdal <andreasr@src.gnome.org>
2006-09-29 21:06:48 +0000

    Add translatable strings from GGZ.

---
+++ libgames-support/ggz-strings.h
@@ -0,0 +1,1202 @@
+/* Strings from ggz-gtk. See README.TRANSLATORS for information. */
+
+gchar *s = N_("About")
+
+
+
+gchar *s = N_("/msg <username> <message> . Private message a player")
+
+
+
+gchar *s = N_("/table <message> .......... Message to your table")
+
+
+
+gchar *s = N_("/wall <message> ........... Admin command")
+
+
+
+gchar *s = N_("/beep <username> .......... Beep a player")
+
+
+
+gchar *s = N_("/help ..................... Get help")
+
+
+
+gchar *s = N_("/friends .................. List your friends")
+
+
+
+gchar *s = N_("/ignore ................... List people you're ignoring")
+
+
+
+gchar *s = N_("You have received an unknown message from %s.")
+
+
+
+gchar *s = N_("You've been beeped by %s.")
+
+
+
+gchar *s = N_("Usage: /msg <username> <message>")
+
+
+
+gchar *s = N_("    Sends a private message to a user on the network.")
+
+
+
+gchar *s = N_("Beep sent to %s.")
+
+
+
+gchar *s = N_("%s (logged on)")
+
+
+
+gchar *s = N_("%s (logged off)")
+
+
+
+gchar *s = N_("Chat Commands")
+
+
+
+gchar *s = N_("-------------")
+
+
+
+gchar *s = N_("/me <action> .............. Send an action")
+
+
+
+gchar *s = N_("Added %s to your friends list.")
+
+
+
+gchar *s = N_("Removed %s from your friends list.")
+
+
+
+gchar *s = N_("Added %s to your ignore list.")
+
+
+
+gchar *s = N_("Removed %s from your ignore list.")
+
+
+
+gchar *s = N_("People currently your friends")
+
+
+
+gchar *s = N_("People you're currently ignoring")
+
+
+
+gchar *s = N_("Multiple matches:")
+
+
+
+gchar *s = N_("Are you sure you want to quit?")
+
+
+
+gchar *s = N_("Quit?")
+
+
+
+gchar *s = N_("Server stats are not implemented yet. If\n"
+
+"you would like to help head over to\n"
+
+"http://www.ggzgamingzone.org/")
+
+
+
+gchar *s = N_("Not Implemented")
+
+
+
+gchar *s = N_("Player stats are not implemented yet. If\n"
+
+"you would like to help head over to\n"
+
+"http://www.ggzgamingzone.org/")
+
+
+
+gchar *s = N_("GGZ Gaming Zone help needs a browser to be configured.\n"
+
+"The configuration dialog will be invoked now.")
+
+
+
+gchar *s = N_("Inline game help is not implemented yet. Help\n"
+
+"is on our website. If\n"
+
+"you would like to help head over to\n"
+
+"http://www.ggzgamingzone.org/")
+
+
+
+gchar *s = N_("You must highlight a table before you can join it.")
+
+
+
+gchar *s = N_("Error Joining")
+
+
+
+gchar *s = N_("That table is full.")
+
+
+
+gchar *s = N_("Error launching game module.")
+
+
+
+gchar *s = N_("Game Error")
+
+
+
+gchar *s = N_("You must highlight a table before you can watch it.")
+
+
+
+gchar *s = N_("Error Spectating")
+
+
+
+gchar *s = N_("Failed to join table.\n"
+
+"Join aborted.")
+
+
+
+gchar *s = N_("Join Error")
+
+
+
+gchar *s = N_("Disconnect from the GGZ Gaming Zone server")
+
+
+
+gchar *s = N_("Start playing a game at a new table")
+
+
+
+gchar *s = N_("Join an existing game")
+
+
+
+gchar *s = N_("Watch an existing game - become a spectator of the table")
+
+
+
+gchar *s = N_("Leave the game you're currently playing")
+
+
+
+gchar *s = N_("Show the properties dialog to change the client settings")
+
+
+
+gchar *s = N_("Show the game stats for the current room's game type")
+
+
+
+gchar *s = N_("Exit the GGZ client application.")
+
+
+
+gchar *s = N_("Client Version:14 %s")
+
+
+
+gchar *s = N_("GTK+ Version:14 %d.%d.%d\n")
+
+
+
+gchar *s = N_("Compiled with debugging.")
+
+
+
+gchar *s = N_("GGZ")
+
+
+
+gchar *s = N_("Connect")
+
+
+
+gchar *s = N_("Disconnect")
+
+
+
+gchar *s = N_("Quit")
+
+
+
+gchar *s = N_("Game")
+
+
+
+gchar *s = N_("Launch")
+
+
+
+gchar *s = N_("Join")
+
+
+
+gchar *s = N_("Watch")
+
+
+
+gchar *s = N_("Leave")
+
+
+
+gchar *s = N_("Edit")
+
+
+
+gchar *s = N_("Properties")
+
+
+
+gchar *s = N_("View")
+
+
+
+gchar *s = N_("Room List")
+
+
+
+gchar *s = N_("Player List")
+
+
+
+gchar *s = N_("Server Stats")
+
+
+
+gchar *s = N_("Player Stats")
+
+
+
+gchar *s = N_("Game Types")
+
+
+
+gchar *s = N_("MOTD")
+
+
+
+gchar *s = N_("Help")
+
+
+
+gchar *s = N_("Copyright")
+
+
+
+gchar *s = N_("GGZ Help")
+
+
+
+gchar *s = N_("Game Help")
+
+
+
+gchar *s = N_("Goto Web")
+
+
+
+gchar *s = N_("Stats")
+
+
+
+gchar *s = N_("Current Room:")
+
+
+
+gchar *s = N_("Message:")
+
+
+
+gchar *s = N_("Send")
+
+
+
+gchar *s = N_("GGZ Gaming Zone")
+
+
+
+gchar *s = N_("Wizard")
+
+
+
+gchar *s = N_("Deity")
+
+
+
+gchar *s = N_("Sentinel")
+
+
+
+gchar *s = N_("Captain")
+
+
+
+gchar *s = N_("Knight")
+
+
+
+gchar *s = N_("Angel")
+
+
+
+gchar *s = N_("Silverlord")
+
+
+
+gchar *s = N_("Eagle")
+
+
+
+gchar *s = N_("Vampire")
+
+
+
+gchar *s = N_("Chief")
+
+
+
+gchar *s = N_("Colonel")
+
+
+
+gchar *s = N_("General")
+
+
+
+gchar *s = N_("Major")
+
+
+
+gchar *s = N_("Scout")
+
+
+
+gchar *s = N_("Lieutenant")
+
+
+
+gchar *s = N_("Stalker")
+
+
+
+gchar *s = N_("Scientist")
+
+
+
+gchar *s = N_("Scholar")
+
+
+
+gchar *s = N_("Entity")
+
+
+
+gchar *s = N_("Creator")
+
+
+
+gchar *s = N_("GGZ Community (fast)")
+
+
+
+gchar *s = N_("Local developer server")
+
+
+
+gchar *s = N_("First Time Configuration")
+
+
+
+gchar *s = N_("This is the first time you are running the GTK+ GGZ Gaming Zone client. "
+
+"Would you like to create some default server profiles?")
+
+
+
+gchar *s = N_("You don't have this game installed. You can download\n"
+
+"it from %s.")
+
+
+
+gchar *s = N_("Launch Error")
+
+
+
+gchar *s = N_("Failed to execute game module.\n"
+
+" Launch aborted.")
+
+
+
+gchar *s = N_("Launched game")
+
+
+
+gchar *s = N_("Launch failed")
+
+
+
+gchar *s = N_("You can only play one game at a time.")
+
+
+
+gchar *s = N_("You're still at a table.")
+
+
+
+gchar *s = N_("You must be in a room to launch a game.\n"
+
+"Launch aborted")
+
+
+
+gchar *s = N_("No game types defined for this server.\n"
+
+"Launch aborted.")
+
+
+
+gchar *s = N_("This game doesn't support spectators.")
+
+
+
+gchar *s = N_("You need to launch the GGZ client directly\n"
+
+"to be able to play this game.")
+
+
+
+gchar *s = N_("Error connecting to server: %s")
+
+
+
+gchar *s = N_("Error")
+
+
+
+gchar *s = N_("Your new password is %s")
+
+
+
+gchar *s = N_("New password")
+
+
+
+gchar *s = N_("Players on server: %d")
+
+
+
+gchar *s = N_("Current Room: %s")
+
+
+
+gchar *s = N_("You've joined room \"%s\".")
+
+
+
+gchar *s = N_("Error joining room: %s")
+
+
+
+gchar *s = N_("You can't chat while not in a room.")
+
+
+
+gchar *s = N_("You don't have permission to chat here.")
+
+
+
+gchar *s = N_("No private chatting at a table!")
+
+
+
+gchar *s = N_("That player isn't in the room!")
+
+
+
+gchar *s = N_("There was an error sending the chat.")
+
+
+
+gchar *s = N_("You're not at a table.")
+
+
+
+gchar *s = N_("Chat failed: %s.")
+
+
+
+gchar *s = N_("Error launching table: %s")
+
+
+
+gchar *s = N_("You have joined table %d.")
+
+
+
+gchar *s = N_("Error joining table: %s")
+
+
+
+gchar *s = N_("You have been booted from the table by %s.")
+
+
+
+gchar *s = N_("You have left the table.")
+
+
+
+gchar *s = N_("The game is over.")
+
+
+
+gchar *s = N_("There was an error with the game server.")
+
+
+
+gchar *s = N_("Error leaving table: %s")
+
+
+
+gchar *s = N_("**none**")
+
+
+
+gchar *s = N_("Offline")
+
+
+
+gchar *s = N_("Connecting")
+
+
+
+gchar *s = N_("Reconnecting")
+
+
+
+gchar *s = N_("Online")
+
+
+
+gchar *s = N_("Logging In")
+
+
+
+gchar *s = N_("Logged In")
+
+
+
+gchar *s = N_("--> Room")
+
+
+
+gchar *s = N_("Chatting")
+
+
+
+gchar *s = N_("--> Table")
+
+
+
+gchar *s = N_("Playing")
+
+
+
+gchar *s = N_("<-- Table")
+
+
+
+gchar *s = N_("Logging Out")
+
+
+
+gchar *s = N_("Server error: %s")
+
+
+
+gchar *s = N_("Disconnected from server.")
+
+
+
+gchar *s = N_("Game Type:  %s")
+
+
+
+gchar *s = N_("Author:  %s")
+
+
+
+gchar *s = N_("Description:  %s")
+
+
+
+gchar *s = N_("Home Page:  %s")
+
+
+
+gchar *s = N_("Failed to launch table.\n"
+
+" Launch aborted.")
+
+
+
+gchar *s = N_("Invalid number of bots specified")
+
+
+
+gchar *s = N_("Seat Assignments")
+
+
+
+gchar *s = N_("Game Type:")
+
+
+
+gchar *s = N_("Number of seats")
+
+
+
+gchar *s = N_("Author:")
+
+
+
+gchar *s = N_("Description:")
+
+
+
+gchar *s = N_("Homepage:")
+
+
+
+gchar *s = N_("Seat %d:")
+
+
+
+gchar *s = N_("Computer")
+
+
+
+gchar *s = N_("Open")
+
+
+
+gchar *s = N_("Reserved for")
+
+
+
+gchar *s = N_("Game Description   ")
+
+
+
+gchar *s = N_("Login")
+
+
+
+gchar *s = N_("Sorry!")
+
+
+
+gchar *s = N_("That username is already in usage.")
+
+
+
+gchar *s = N_("Authentication has failed.\n"
+
+"Please supply the correct password.")
+
+
+
+gchar *s = N_("The username is too long!")
+
+
+
+gchar *s = N_("Invalid username, do not use special characters!")
+
+
+
+gchar *s = N_("Login failed for unknown reason: %s")
+
+
+
+gchar *s = N_("Connect and Login")
+
+
+
+gchar *s = N_("Server Profile")
+
+
+
+gchar *s = N_("Profile:")
+
+
+
+gchar *s = N_("Edit Profiles...")
+
+
+
+gchar *s = N_("Server:")
+
+
+
+gchar *s = N_("localhost")
+
+
+
+gchar *s = N_("Port:")
+
+
+
+gchar *s = N_("5688")
+
+
+
+gchar *s = N_("Username:")
+
+
+
+gchar *s = N_("Password:")
+
+
+
+gchar *s = N_("Email:")
+
+
+
+gchar *s = N_("Normal Login")
+
+
+
+gchar *s = N_("Guest Login")
+
+
+
+gchar *s = N_("First-time Login")
+
+
+
+gchar *s = N_("Enable session log")
+
+
+
+gchar *s = N_("URL for autoconnection")
+
+
+
+gchar *s = N_("GGZ Core Client for Gtk+")
+
+
+
+gchar *s = N_("Wrong arguments, try %s --help.\n")
+
+
+
+gchar *s = N_("Message of the Day")
+
+
+
+gchar *s = N_("Which client would you like to use to play this game?")
+
+
+
+gchar *s = N_("Don't ask me again.")
+
+
+
+gchar *s = N_("Player Information")
+
+
+
+gchar *s = N_("Player Handle:")
+
+
+
+gchar *s = N_("Table:")
+
+
+
+gchar *s = N_("Account:")
+
+
+
+gchar *s = N_("Record:")
+
+
+
+gchar *s = N_("Rating:")
+
+
+
+gchar *s = N_("Rank:")
+
+
+
+gchar *s = N_("Score:")
+
+
+
+gchar *s = N_("Unknown")
+
+
+
+gchar *s = N_("Registered")
+
+
+
+gchar *s = N_("Guest")
+
+
+
+gchar *s = N_("Administrator")
+
+
+
+gchar *s = N_("Bot")
+
+
+
+gchar *s = N_("Info")
+
+
+
+gchar *s = N_("Friends")
+
+
+
+gchar *s = N_("Ignore")
+
+
+
+gchar *s = N_("#%d")
+
+
+
+gchar *s = N_("L")
+
+
+
+gchar *s = N_("T#")
+
+
+
+gchar *s = N_("Name")
+
+
+
+gchar *s = N_("Properties Updated")
+
+
+
+gchar *s = N_("Confirm:")
+
+
+
+gchar *s = N_("Modify")
+
+
+
+gchar *s = N_("Servers")
+
+
+
+gchar *s = N_("Chat Font:")
+
+
+
+gchar *s = N_("Change")
+
+
+
+gchar *s = N_("Ignore Join/Part Messages")
+
+
+
+gchar *s = N_("Play Sounds")
+
+
+
+gchar *s = N_("Auto Indent")
+
+
+
+gchar *s = N_("Timestamp Chats")
+
+
+
+gchar *s = N_("Word Wrap")
+
+
+
+gchar *s = N_("Chat Color")
+
+
+
+gchar *s = N_("Default chat color assigned to your friends")
+
+
+
+gchar *s = N_("Chat color used when your name is typed")
+
+
+
+gchar *s = N_("Chat color used for all other chats")
+
+
+
+gchar *s = N_("Normal Color")
+
+
+
+gchar *s = N_("Highlight Color")
+
+
+
+gchar *s = N_("Friend Color")
+
+
+
+gchar *s = N_("Black Background")
+
+
+
+gchar *s = N_("White Background")
+
+
+
+gchar *s = N_("Chat")
+
+
+
+gchar *s = N_("All of the following information is optional.")
+
+
+
+gchar *s = N_("Name:")
+
+
+
+gchar *s = N_("City:")
+
+
+
+gchar *s = N_("State:")
+
+
+
+gchar *s = N_("Country:")
+
+
+
+gchar *s = N_("Comments, Hobies, Etc.")
+
+
+
+gchar *s = N_("User Information")
+
+
+
+gchar *s = N_("Browser to launch URLs with:")
+
+
+
+gchar *s = N_("Galeon - New")
+
+
+
+gchar *s = N_("Galeon - Existing")
+
+
+
+gchar *s = N_("Gnome URL Handler")
+
+
+
+gchar *s = N_("Konqueror - New")
+
+
+
+gchar *s = N_("Konqueror - Existing")
+
+
+
+gchar *s = N_("Lynx")
+
+
+
+gchar *s = N_("Mozilla - New")
+
+
+
+gchar *s = N_("Mozilla - Existing")
+
+
+
+gchar *s = N_("Netscape - New")
+
+
+
+gchar *s = N_("Netscape - Existing")
+
+
+
+gchar *s = N_("Opera - New")
+
+
+
+gchar *s = N_("Opera - Existing")
+
+
+
+gchar *s = N_("Firefox - New")
+
+
+
+gchar *s = N_("Firefox - Existing")
+
+
+
+gchar *s = N_("Single Click Room Entry")
+
+
+
+gchar *s = N_("Display All")
+
+
+
+gchar *s = N_("Display New")
+
+
+
+gchar *s = N_("Display Important")
+
+
+
+gchar *s = N_("Display None")
+
+
+
+gchar *s = N_("Options")
+
+
+
+gchar *s = N_("Select Font")
+
+
+
+gchar *s = N_("Room Information")
+
+
+
+gchar *s = N_("Game Name:")
+
+
+
+gchar *s = N_("Room Description:")
+
+
+
+gchar *s = N_("This room has no game")
+
+
+
+gchar *s = N_("N/A")
+
+
+
+gchar *s = N_("Unknown room")
+
+
+
+gchar *s = N_("You can't join a room; you're not logged in")
+
+
+
+gchar *s = N_("You're already in between rooms")
+
+
+
+gchar *s = N_("You can't switch rooms while playing a game")
+
+
+
+gchar *s = N_("Unknown error")
+
+
+
+gchar *s = N_("Error joining room")
+
+
+
+gchar *s = N_("Room")
+
+
+
+gchar *s = N_("No description available.")
+
+
+
+gchar *s = N_("Seats")
+
+
+
+gchar *s = N_("Description")
+
+
+
+gchar *s = N_("None")
+
+
+
+gchar *s = N_("Room filtering is not implemented yet. If\n"
+
+"you would like to help head over to\n"
+
+"http://www.ggzgamingzone.org/")
+
+
+
+gchar *s = N_("Web Address")
+
+
+
+gchar *s = N_("Author")
+
+
+
+gchar *s = N_("Room List Filter:")
+
+
+
+gchar *s = N_("Set")
+
+
+
+gchar *s = N_("Upgrage Game")
+
+
+
+gchar *s = N_("Goto Web Site")
+
