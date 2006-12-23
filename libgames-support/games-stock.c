030cd0eb4e4118cf56df7e9a405557eea7d65f9c
Francisco Javier F. Serrador <serrador@openshine.com>
2006-12-23 18:58:42 +0000

    Updated FSF snail mail address.
    006-12-23  Francisco Javier F. Serrador  <serrador@openshine.com>
    
    	* games-stock.c: Updated FSF snail mail address.

--- libgames-support/games-stock.c
+++ libgames-support/games-stock.c
@@ -345,7 +345,7 @@ games_get_license (const gchar * game_name)
        "GNU General Public License for more details."),
     N_("You should have received a copy of the GNU General Public License "
        "along with %s; if not, write to the Free Software Foundation, Inc., "
-       "59 Temple Place, Suite 330, Boston, MA  02111-1307  USA")
+       "51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA")
   };
 
   license_trans = g_strjoin ("\n\n", _(license[0]), _(license[1]),
