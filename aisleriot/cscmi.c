dd1b3f7cb11a40d2eaedd771df2d94b326de09e3
Robert Ancell <rancell@src.gnome.org>
2007-04-25 07:46:00 +0000

    Fix read-after-free (Bug #423459)
    svn path=/trunk/; revision=6166

--- aisleriot/cscmi.c
+++ aisleriot/cscmi.c
@@ -637,8 +637,8 @@ cscmi_drop_cards (int start_slot, GList * cards, int end_slot)
 			    cscmi_call_lambda,
 			    call_data, cscmi_catch_handler, NULL);
   retval = call_data->retval;
-  g_free (call_data);
   scm_gc_unprotect_object (call_data->arg2);
+  g_free (call_data);
 
   return SCM_NFALSEP (retval);
 }
