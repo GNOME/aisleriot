5fd39d2daa78475daa4315a88c5667a215d26a2c
Jason D. Clinton <me@jasonclinton.com>
2009-10-26 16:46:30 -0500

    autogen.sh: Oddly, specifically looks for a handful of directories for sanity

--- autogen.sh
+++ autogen.sh
@@ -8,7 +8,7 @@ PKG_NAME="Gnome Games"
 
 (test -f $srcdir/configure.in \
   && test -d $srcdir/gnomine \
-  && test -d $srcdir/same-gnome) || {
+  && test -d $srcdir/swell-foop) || {
     echo -n "**Error**: Directory \"\`$srcdir\'\" does not look like the"
     echo " top-level gnome directory"
     exit 1
