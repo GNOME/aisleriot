fd5c66e261d85ee95f8a2594145b539953aceae3
Christian Persch <chpe@gnome.org>
2010-01-04 19:55:37 +0100

    libgames-support: It's 2010!

--- libgames-support/games-glib-compat.h
+++ libgames-support/games-glib-compat.h
@@ -1,5 +1,6 @@
 /*
  *  Copyright © 2009 Thomas H.P. Andersen <phomes@gmail.com>
+ *  Copyright © 2010 Christian Persch
  *
  *  This runtime is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published by
