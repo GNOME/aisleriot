8cc3a8a2da40c35c90813c19a2055eaddf650a61
Thomas Hindoe Paaboel Andersen <phomes@gmail.com>
2010-03-11 18:45:29 +0100

    GSEAL: window is already available directly. Bug #586731

--- aisleriot/board-noclutter.c
+++ aisleriot/board-noclutter.c
@@ -2378,8 +2378,8 @@ aisleriot_board_realize (GtkWidget *widget)
 #ifndef HAVE_HILDON 
   /* Create cursors */
   priv->cursor[AR_CURSOR_DEFAULT] = gdk_cursor_new_for_display (display, GDK_LEFT_PTR);
-  priv->cursor[AR_CURSOR_OPEN] = ar_cursor_new (widget->window, AR_CURSOR_OPEN);
-  priv->cursor[AR_CURSOR_CLOSED] = ar_cursor_new (widget->window, AR_CURSOR_CLOSED);
+  priv->cursor[AR_CURSOR_OPEN] = ar_cursor_new (window, AR_CURSOR_OPEN);
+  priv->cursor[AR_CURSOR_CLOSED] = ar_cursor_new (window, AR_CURSOR_CLOSED);
   priv->cursor[AR_CURSOR_DROPPABLE] = gdk_cursor_new_for_display (display, GDK_DOUBLE_ARROW); /* FIXMEchpe: better cursor */
 #endif /* !HAVE_HILDON */
 
