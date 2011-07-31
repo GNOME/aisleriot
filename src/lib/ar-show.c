bb607a48f79ce593e86c65199505d1e806ce60fb
Christian Persch <chpe@gnome.org>
2011-07-31 19:27:01 +0200

    lib: Go GPL3+
    Make all our own code GPL3+ and keep only copypasted code under
    LGPL.

--- src/lib/ar-show.c
+++ src/lib/ar-show.c
@@ -2,19 +2,18 @@
  *  Copyright © 2008 Thomas H.P. Andersen <phomes@gmail.com>
  *  Copyright © 2007, 2008, 2009 Christian Persch
  *
- *  This runtime is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as published by
- *  the Free Software Foundation; either version 3, or (at your option)
- *  any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *  This runtime is distributed in the hope runtime it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with this runtime; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <config.h>
